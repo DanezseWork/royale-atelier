46a293800eb69015d6351aeadd09650786e2fe72
DanezseWork <dpjose.work@gmail.com>
2026-04-15 13:42:53 +0800

    feat(about): implement about section with assets and custom font styling

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,7 +1,9 @@
 export function About(){
     return(
-        <div className="bg-[#f1ebe3] min-h-screen">
-            About Us
+        <div className="bg-[#f1ebe3] font-boston min-h-screen flex items-center relative">
+            <img src="/images/about-pic.png" className="absolute right-10 h-70 -rotate-5" />
+            <span className="absolute left-25 top-83 w-95 text-center rotate-4">Founded by cosplayer Cora San in 2023, Royale Atelier (formerly Regium Cosplay Services) grew from small projects into a dedicated space for wig styling and cosplay rentals. Alongside Tiara’s Wig Styling, the brand is known for its fluffy, natural finish and airbrushed look, creating character-accurate wigs for passionate cosplayers.</span>
+            <img src="/images/about-container.png" />
         </div>
     )
 }
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -5,7 +5,7 @@ export function Hero() {
       style={{ backgroundImage: "url('/images/hero-bg.jpg')" }}
     >
       <img src="/images/hero-title.png" className="lg:h-150" alt="Hero title" />
-      <img className="absolute w-full -bottom-20 lg:-bottom-70" src="/images/hero-pattern.png" />
+      <img className="absolute w-full -bottom-20 lg:-bottom-70 z-20" src="/images/hero-pattern.png" />
     </section>
   );
 }
